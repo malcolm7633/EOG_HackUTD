baa73d469ff805111e48255390939dd3548f0c56
malcolm7633 <94253924+malcolm7633@users.noreply.github.com>
2021-11-13 15:18:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from websocket import create_connection
-ws = create_connection("wss://2021-utd-hackathon.azurewebsites.net")
-ws.send("Hello, World")
-result = ws.recv()
-print(result)
-ws.close()
+import websocket
+
+def on_message(wsapp, message):
+    print(message)
+
+wsapp = websocket.WebSocketApp("wss://2021-utd-hackathon.azurewebsites.net", on_message=on_message)
+wsapp.run_forever()
